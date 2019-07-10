904477b2ad2fbc15e488cfb13a7989a634267da1
katlin anderson and kaya jepson <kaya@>
2019-07-10 13:40:33 -0700

    add button expire to spellsclass

--- src/components/SpellsClass.jsx
+++ src/components/SpellsClass.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import Expire from 'react-expire';
 
 function SpellsClass(props){
   let count = 0;
@@ -22,8 +23,13 @@ function SpellsClass(props){
     <div id="spells">
     <h5>Today you will be learning a newspell. </h5>
     <h6>To master this spell, you must click this button 15 times in 10 seconds.</h6>
-    <button id="spell-button" onClick={incrementCount}>Try spell</button>
-    <Link to="/puzzle"><button id="complete-class" onClick={assessClassPassed}>Complete class</button></Link>
+    <Expire until={10000}>
+      {(expired) => (
+      expired ? <Link to="/puzzle"><button id="complete-class" onClick={assessClassPassed}>Complete class</button></Link> : <button id="spell-button" onClick={incrementCount}>Try spell</button>
+      )}
+    </Expire>
+    
+    
 
     </div>
   );
